4b1e185dc2ab55bd5be86496076c3af75a340982
Christian-39 <ezechristian1999@gmail.com>
2026-02-26 23:12:26 +0000

    db issues

--- chris_lux/settings/development.py
+++ chris_lux/settings/development.py
@@ -9,7 +9,7 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'chris-lux.onrender.com', '*']
+ALLOWED_HOSTS = ['localhost', '105.112.216.179', 'chris-lux.onrender.com', '*']
 
 # Database
 DATABASES =  {
