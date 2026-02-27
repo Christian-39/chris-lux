24772bb0f52c5aff4c9a0161fa86ebf495cf805f
Christian-39 <ezechristian1999@gmail.com>
2026-02-27 07:57:36 +0000

    postgree

--- chris_lux/settings/development.py
+++ chris_lux/settings/development.py
@@ -15,8 +15,12 @@ environ.Env.read_env()
 DEBUG = False
 
 # Database
-DATABASES =  {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Static files - development
