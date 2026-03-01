ff787f6a56bfca91fb8c42d7a77c6c010ae95301
Christian-39 <ezechristian1999@gmail.com>
2026-03-01 18:09:04 +0000

    new

--- chris_lux/urls.py
+++ chris_lux/urls.py
@@ -3,14 +3,14 @@ URL configuration for Chris Lux project.
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 from chris_lux.core.sitemaps import StaticViewSitemap, ProductSitemap
-import re
 from django.views.static import serve
+
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -37,12 +37,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# This is the "Force Serve" block for Render
-if not settings.DEBUG:
     urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
-else:
-    # Standard local development serving
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),]
