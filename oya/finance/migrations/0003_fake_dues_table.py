3a20c75ec0e1d8343d78d0f94efb16ca71e877b0
Christian-39 <111363863+Christian-39@users.noreply.github.com>
2026-07-03 15:24:02 +0100

    Update 0003_fake_dues_table.py

--- oya/finance/migrations/0003_fake_dues_table.py
+++ oya/finance/migrations/0003_fake_dues_table.py
@@ -3,10 +3,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        # Link it straight to 0001 to bypass the 0002 node error
-        ('finance', '0001_initial'),
+        ('finance', '0002_income_add_member'),
     ]
 
     operations = [
-        # Kept completely empty so it doesn't create any tables
+        # Kept completely empty
     ]
