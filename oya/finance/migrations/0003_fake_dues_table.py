65a33349189a5537ac2c2dc50100ef7b140abe2d
Christian-39 <111363863+Christian-39@users.noreply.github.com>
2026-07-03 15:06:00 +0100

    Update 0003_fake_dues_table.py

--- oya/finance/migrations/0003_fake_dues_table.py
+++ oya/finance/migrations/0003_fake_dues_table.py
@@ -5,7 +5,7 @@ class Migration(migrations.migrations.Migration):
     dependencies = [
         # CRITICAL: Replace '0003_previous_file_name' with the exact name 
         # of your last migration file in this folder (minus the .py extension)
-        ('finance', '0003_previous_file_name'),
+        ('finance', '0002_income_add_member'),
     ]
 
     operations = [
